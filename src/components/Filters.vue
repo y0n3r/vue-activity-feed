933b49163d2d2b4767d84f47a9248c9b1d62fb4c
Yonas Hassen <yonas1@gmail.com>
2018-10-21 21:15:31 -0400

    Adds <Filters> component and ties it into router.

---
+++ src/components/Filters.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="filters">
+    <h2 class="filters__title">Filters</h2>
+    <ul class="filters__filters">
+      <li><router-link to="/">Activity</router-link></li>
+      <li><router-link to="/recipes">Recipes</router-link></li>
+      <li><router-link to="/photos">Photos</router-link></li>
+      <li><router-link to="/reviews">Reviews</router-link></li>
+      <li><router-link to="/tweaks">Tweaks</router-link></li>
+      <li><router-link to="/questions">Questions</router-link></li>
+      <li><router-link to="/follows">Follows</router-link></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss">
+
+</style>
