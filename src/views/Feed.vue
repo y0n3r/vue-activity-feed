43a887976453537933b4eda92001f739adc0ef94
Yonas Hassen <yonas1@gmail.com>
2018-10-30 19:06:33 -0400

    Renames the feed container.

---
+++ src/views/Feed.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="feed">
+    <loading v-if="items.length === 0"/>
+  </div>
+</template>
+
+<script>
+import Loading from '@/components/Loading';
+import API from '@/constants/api';
+import http from '@/api';
+
+export default {
+  name: 'feed',
+  data() {
+    return {
+      items: []
+    }
+  },
+  components: {
+    Loading
+  },
+  created() {
+    this.fetchItems();
+  },
+  methods: {
+    fetchItems() {
+
+    }
+  },
+  computed: {
+    
+  }
+};
+</script>
