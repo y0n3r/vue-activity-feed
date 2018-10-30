76957ffc1d6753fed04b01be9b41c95b82c3ae3f
Yonas Hassen <yonas1@gmail.com>
2018-10-30 19:05:41 -0400

    Updates all routes to render the Feed.vue container.

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Feed from './views/Feed.vue';
 
 Vue.use(Router);
 
@@ -10,16 +10,50 @@ export default new Router({
       path: '/',
       name: 'home',
       redirect: '/activity',
-      component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: 'about' */ './views/About.vue')
+      path: '/activity',
+      name: 'activity',
+      component: Feed
+    },
+    {
+      path: '/recipes',
+      name: 'recipes',
+      component: Feed
+    },
+    {
+      path: '/photos',
+      name: 'photos',
+      component: Feed
+    },
+    {
+      path: '/reviews',
+      name: 'reviews',
+      component: Feed
+    },
+    {
+      path: '/tweaks',
+      name: 'tweaks',
+      component: Feed
+    },
+    {
+      path: '/questions',
+      name: 'questions',
+      component: Feed
+    },
+    {
+      path: '/follows',
+      name: 'follows',
+      component: Feed
     }
+    //{
+    //  path: '/about',
+    //  name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    //  component: () =>
+    //    import(/* webpackChunkName: 'about' */ './views/About.vue')
+    //}
   ]
 });
