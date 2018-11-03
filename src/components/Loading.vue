0b4a4aad2e479d954a969894b06b2ccdb2fcccb6
Yonas Hassen <yonas1@gmail.com>
2018-11-03 18:40:16 -0400

    Spruces up the loading spinner.

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="loading">
-    Loading...
+    <svg width="73px"  height="73px"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="lds-rolling" style="background: none;"><circle cx="50" cy="50" fill="none" stroke="#892074" stroke-width="15" r="35" stroke-dasharray="164.93361431346415 56.97787143782138" transform="rotate(58.1688 50 50)"><animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 50;360 50 50" keyTimes="0;1" dur="0.6s" begin="0s" repeatCount="indefinite"></animateTransform></circle></svg>
   </div>
 </template>
 
-<script>
-export default {
-
+<style lang="scss">
+.loading {
+  text-align: center;
 }
-</script>
-
-<style>
-
 </style>
